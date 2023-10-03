bb6bbaaaa53d1b241849ae7395f604c0cb9dca7c
Matheus Cristofoli <matfrietasc@gmail.com>
2023-10-02 22:22:12 -0400

    I am trying jsdocs....

---
+++ app.js
@@ -0,0 +1,13 @@
+// This is the main function that will be called by the index.js file
+
+const placeOrders = () => {}
+
+/**
+ * This function will take in an object containing the flavors and the quantity of each flavor to be ordered
+ * @param {Object.<string, number>} flavors - An object containing the flavors and the quantity of each flavor to be ordered
+ * @returns {flavors} - An object containing the flavors and the quantity of each flavor that was ordered
+ */
+
+const countFlavors = (flavors) => {
+	// Your code here
+}
